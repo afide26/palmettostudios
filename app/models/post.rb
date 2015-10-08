b8a84bae32a1bd5ef1adba01eedbdb54dfc59ede
Alan Fidelino <afidedev26@gmail.com>
2015-10-08 21:55:01 +1100

    Removed Dropbox auto save settings for development

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,15 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true
 	validates :body, length: {minimum: 10}
-	has_attached_file :image, styles: { large:"400X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg",
+
+	if Rails.env.development?
+		has_attached_file :image, styles: { large:"400X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg"
+	else
+	    has_attached_file :image, styles: { large:"400X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg",
 				      :storage => :dropbox,
 				      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	end
+
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 									   
 end
