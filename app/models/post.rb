f122c62c1e66996c561d65c205a2d752ec46edb8
Alan Fidelino <afidedev26@gmail.com>
2015-10-18 23:20:25 +1100

    Changed css for images

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ActiveRecord::Base
 	validates :body, length: {minimum: 10}
 
 	if Rails.env.development?
-		has_attached_file :image, styles: { large:"400X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg"
+		has_attached_file :image, styles: { large:"500X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg"
 	else
-	    has_attached_file :image, styles: { large:"400X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg",
+	    has_attached_file :image, styles: { large:"500X400>", medium:"250x250>" }, default_url: "/images/:style/missing.jpg",
 				      :storage => :dropbox,
 				      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	end
